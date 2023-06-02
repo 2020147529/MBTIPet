834f62ea7033831cffa47ea25a0db942f218364a
2020134032 <junejeong@yonsei.ac.kr>
2023-06-02 18:01:28 +0900

    mbti별 출력/mbtito16.json파일 추가/css 일부 변경

--- SCRIPT/dog_result.js
+++ SCRIPT/dog_result.js
@@ -1,14 +1,27 @@
-let recievedMBTI = location.href.split('?')[1] // url에 있는 mbti값을 받음(1페이지에서 전송)  
+let recievedMBTI = location.href.split('?')[1]; // url에 있는 mbti값을 받음(1페이지에서 전송)  
 
-updateSectionInfo(recievedMBTI);
+(async function (){
+   
+  const data_mbtitodog = await fetchjson("../SCRIPT/mbtito16.json");
 
-function updateSectionInfo(mbtiString) {
+  updateSectionInfo(data_mbtitodog, recievedMBTI);
+
+})();//메인 실행부
+
+async function fetchjson(url){
+  const res = await fetch(url);
+  const data = await res.json(); 
+  return data
+}
+
+
+function updateSectionInfo(data_mbtitodog, mbtiString) {
   //메인 컨텐츠 MBTI parameter에 맞추어 display
   // let doginfo = getDogInfo(mbtiString);
-
-  let dogName = mbtiString ;
-  let imageSource = "https://picsum.photos/500"; //todo: 추후데이터 사용
-  let description = "doginfo[1]";
+  let data = data_mbtitodog[mbtiString]
+  let dogName = data.species ;
+  let imageSource = "../IMAGE/"+dogName+".jpg"; //저장된 이미지 사용
+  let description = mbtiString.toUpperCase() +":"+data.info;
 
   let result = `<h2>${dogName}</h2>
     <div class="imgcontainer">
